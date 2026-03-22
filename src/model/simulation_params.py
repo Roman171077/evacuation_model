0166470ceb28aa2b20c5ed9a8230a08c982e3895
Roman <sokolovrv77@gmail.com>
2026-03-22 14:26:35 +0800

    v2.2

--- src/model/simulation_params.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import dataclass
-
-
-@dataclass
-class SimulationParams:
-    dt: float = 0.1
-    max_time: float = 3600.0
-    winter_clothing: bool = False
-    queue_priority_most_negative_first: bool = True
